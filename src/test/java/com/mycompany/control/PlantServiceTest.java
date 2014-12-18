0373a85d8724ebe09fc9bf759b21ce35e47b2e2e
Marianne Krabi <marianne.krabi@movingimage24.com>
2014-12-18 14:02:46 +0100

    commit

--- src/test/java/com/mycompany/control/PlantServiceTest.java
+++ src/test/java/com/mycompany/control/PlantServiceTest.java
@@ -1,16 +1,27 @@
 package com.mycompany.control;
 
+import com.mycompany.entity.Plant;
+import com.mycompany.entity.PlantType;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 
 import javax.persistence.EntityManager;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class PlantServiceTest extends BaseTestClass {
 
     private PlantService plantService;
+
+    private List<Plant> plantList;
+
+    private Plant plant1;
+    private Plant plant2;
+    private Plant plant3;
 
     @Mock
     private EntityManager entityManager;
@@ -18,13 +29,25 @@ public class PlantServiceTest extends BaseTestClass {
     @Before
     public void before() {
         plantService = new PlantService();
-    }
 
+        plant1 = new Plant(PlantType.Daisy, "daisy.gif", 0,0, new Date());
+        plant2 = new Plant(PlantType.Flower, "flower.gif", 1, 1, new Date());
+        plant3 = new Plant(PlantType.Tulip, "tulip.gif", 2, 2, new Date());
+
+        plantList = Arrays.asList(plant1, plant2, plant3);
+    }
 
     @Test
     public void test() {
-        int i = 1;
-        assertEquals(1, i);
+        assertTrue("3 plants inserted in List", plantList.size() == 3);
+
+        for (int i = 0; i<plantList.size(); ++i) {
+            plantService.savePlant(plantList.get(i));
+        }
+
+        assertTrue("Plant1 should be saved", entityManager.contains(plant1));
+        assertTrue("Plant2 should be saved", entityManager.contains(plant2));
+        assertTrue("Plant3 should be saved", entityManager.contains(plant3));
     }
 }
 
